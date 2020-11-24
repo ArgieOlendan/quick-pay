d840383aeaba78b92ec35a214129c1f52251fac6
argie <argieolndn@gmail.com>
2020-11-24 21:43:13 +0800

    prepare for deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { importType } from '@angular/compiler/src/output/output_ast';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+import { OrderComponent } from './order/order.component';
+
+const routes: Routes = [
+  { path: ':productId', component: OrderComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
