9b0eb9c3354c196ab627281072c07e3ef47b4de3
argie <argieolndn@gmail.com>
2020-11-24 19:07:45 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderComponent } from './order/order.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderComponent,
+    ShippingComponent,
+    PaymentComponent,
   ],
   imports: [
     BrowserModule,
